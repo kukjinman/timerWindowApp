df8faf38115e14cd8882f3f85ebb2f9ae8fe63bc
kukjin <thater114@gmail.com>
2023-08-07 22:29:09 +0900

    timer code updated

--- test.py
+++ test.py
@@ -7,13 +7,15 @@ class TimerApp:
         self.root = root
         self.root.title("타이머 앱")
 
-        self.timer_label = tk.Label(root, text="00:00", font=("Helvetica", 48))
+        self.timer_label = tk.Label(root, text="00:00:00", font=("Helvetica", 48))
         self.start_button = tk.Button(root, text="시작", command=self.start_timer)
         self.stop_button = tk.Button(root, text="정지", command=self.stop_timer)
         self.reset_button = tk.Button(root, text="리셋", command=self.reset_timer)
 
         self.timer_running = False
+        self.init_time = None
         self.start_time = None
+        self.stop_time = None
         self.elapsed_time = 0
 
         self.timer_label.pack(pady=20)
@@ -24,18 +26,22 @@ class TimerApp:
     def update_timer(self):
         if self.timer_running:
             current_time = time.time()
-            elapsed_time = current_time - self.start_time + self.elapsed_time
+            if self.stop_time is None:
+                elapsed_time = current_time - self.start_time
+            else:
+                elapsed_time = current_time - self.init_time - (self.start_time - self.stop_time)
+
+            hours = int(elapsed_time / 3600)
             minutes = int(elapsed_time / 60)
             seconds = int(elapsed_time % 60)
-            self.timer_label.config(text=f"{minutes:02d}:{seconds:02d}")
-            self.root.after(1000, self.update_timer)
+            self.timer_label.config(text=f"{hours:02d}:{minutes:02d}:{seconds:02d}")
+            self.root.after(300, self.update_timer)
 
     def start_timer(self):
         if not self.timer_running:
-            if self.start_time is None:
-                self.start_time = time.time()
-            else:
-                self.elapsed_time += time.time() - self.stop_time
+            if self.init_time is None:
+                self.init_time = time.time()
+            self.start_time = time.time()
             self.timer_running = True
             self.update_timer()
 
@@ -48,7 +54,7 @@ class TimerApp:
         self.timer_running = False
         self.start_time = None
         self.elapsed_time = 0
-        self.timer_label.config(text="00:00")
+        self.timer_label.config(text="00:00:00")
 
 if __name__ == "__main__":
     root = tk.Tk()
